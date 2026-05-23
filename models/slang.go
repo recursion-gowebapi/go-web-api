f30e9cdf52989d680912798cc47885effc1a1a80
kokinomu-blip <kokinomu@gmail.com>
2026-05-23 13:08:01 +0900

    fix/format

--- models/slang.go
+++ models/slang.go
@@ -1,19 +1,21 @@
-type Slang struct{
-	ID string `json:"id"`
-	Slang string `json:"slang"`
+package models
+
+type Slang struct {
+	ID       string    `json:"id"`
+	Slang    string    `json:"slang"`
 	Meanings []Meaning `json:"meanings"`
 }
 
-type Meaning struct{
-	MeaningJa string `json:"meaning_ja"`
-	NuanceJa string `json:"nuance_ja"`
-	Examples []Example `json:"examples"`
-	Scene []string `json:"scene"`
-	EmotionCategories []string `json:"emotion_categories"`
-	WarningJa string `json:"warning_ja"`
+type Meaning struct {
+	MeaningJa         string    `json:"meaning_ja"`
+	NuanceJa          string    `json:"nuance_ja"`
+	Examples          []Example `json:"examples"`
+	Scene             []string  `json:"scene"`
+	EmotionCategories []string  `json:"emotion_categories"`
+	WarningJa         string    `json:"warning_ja"`
 }
 
-type Example struct{
+type Example struct {
 	SentenceEn string `json:"sentence_en"`
 	SentenceJa string `json:"sentence_ja"`
 }
