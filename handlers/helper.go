e4cca43462cf0d8fcc0e867d49e11b87bfa139f4
asa129 <asa.jad1290@gmail.com>
2026-05-26 00:25:48 +0900

    refactor: move respondWithError to handlers/helper.go

---
+++ handlers/helper.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, statusCode int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(map[string]string{"error": message})
+}
