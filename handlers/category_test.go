1faca6f5b102f3690c8ebbd4dbf65b93fc23fe68
ShuichiFujii <174758061+ShuichiFujii@users.noreply.github.com.>
2026-05-25 15:29:10 +0900

    TestUniqueCategories関数のテストを作成

--- handlers/category_test.go
+++ handlers/category_test.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"reflect"
+	"github.com/recursion-gowebapi/go-web-api/models"
 )
 
 func TestMethodCheck_OK(t *testing.T) {
@@ -42,3 +44,97 @@ func TestMethodCheck_NG(t *testing.T) {
 	}
 }
 
+func TestValidCategoryType_OK(t *testing.T) {
+	req := httptest.NewRequest(
+		http.MethodGet,
+		"/api/categories/scenes",
+		nil,
+	)
+
+	rr := httptest.NewRecorder()
+
+	validParams := []string{"scenes", "emotion_categories"}
+
+	param, err := validateCategoryType(rr, req, validParams)
+
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+
+	if param != "scenes" {
+		t.Errorf("expected 'scenes', got '%s'", param)
+	}
+}
+
+func TestValidCategoryType_NG(t *testing.T) {
+	req := httptest.NewRequest(
+		http.MethodGet,
+		"/api/categories/invalid",
+		nil,
+	)
+
+	rr := httptest.NewRecorder()
+
+	validParams := []string{"scenes", "emotion_categories"}
+
+	param, err := validateCategoryType(rr, req, validParams)
+
+	if err == nil {
+		t.Error("expected error, got nil")
+	}
+
+	if param != "" {
+		t.Errorf("expected empty string, got '%s'", param)
+	}
+}
+
+func TestUniqueCategories_Empty(t *testing.T) {
+	slangs := []models.Slang{}
+
+	var expected = []string{}
+	result := getUniqueCategories(slangs, "scenes")
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("expected %v, got %v", expected, result)
+	}
+}
+
+func TestGetUniqueCategories_OK(t *testing.T) {
+	slangs := []models.Slang {
+		{
+			ID: "slang_001", 
+			Slang: "lit", 
+			Meanings: []models.Meaning {
+				{
+					MeaningJa: "...", 
+					Scene: []string{"friends", "family", "party", "work", "school"}, 
+					EmotionCategories: []string{"joy", "surprise", "neutral", "other", "unknown"},
+				},
+			},
+		}, 
+		{
+			ID: "slang_002", 
+			Slang: "salty", 
+			Meanings: []models.Meaning {
+				{
+					MeaningJa: "...",
+					Scene: []string{"family", "work", "school", "other", "unknown"},
+					EmotionCategories: []string{"anger", "disgust", "neutral", "other", "unknown"},
+				},
+			},
+		 },
+	}
+
+	expectedScenes := []string{"family", "friends", "other", "party", "school", "unknown", "work"}
+	expectedEmotions := []string{"anger", "disgust", "joy", "neutral", "other", "surprise", "unknown"}
+
+	scenes := getUniqueCategories(slangs, "scenes")
+	emotions := getUniqueCategories(slangs, "emotion_categories")
+
+	if !reflect.DeepEqual(scenes, expectedScenes) {
+		t.Errorf("expected %v, got %v", expectedScenes, scenes)
+	}
+
+	if !reflect.DeepEqual(emotions, expectedEmotions) {
+		t.Errorf("expected %v, got %v", expectedEmotions, emotions)
+	}
+}
